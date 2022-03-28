7a9bd7d5f4ebbaf25e334c6f806c1ed7720625a5
buttertalk <96155679+buttertalk@users.noreply.github.com>
2022-03-28 17:32:25 -0500

    update to style.css from vs

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 // Start your code ***HERE*** =========
 
 // create an array with all possible character types: Uppercase, Lowercase, numbers, and symbols stored in a variable
-
 // create a global variable called "pwLength" with a number between 10 and 18
 
 // Using the above array and password length variable, create a random password using a for loop inside of a function called "addNewPassword" either saved as an arrow function variable or a traditional function
-
+function myFunction() {
+    let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZacdefghijklnopqrstuvwxyz0123456789@#$%^&*";
+  
+  // let letters = "0123456789"
+    password = ""; 
+    
+    for (var i = 0;i < 20;i++) {
+      let generate = letters[Math.floor(Math.random() * 62)];
+      password += generate;
+    }
+    
+       
+    
+  let parent = document.getElementById("password").innerHTML = password;
+  }
+    
 // ========= ⬇ DO NOT TOUCH THIS CODE ⬇ ======
 
 let genBtn = document.getElementById("btnGen");
