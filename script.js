dd422de0cbbad1400c828563c04733b24642c13a
buttertalk <96155679+buttertalk@users.noreply.github.com>
2022-03-29 22:34:20 -0500

    replaced merged files from work

--- script.js
+++ script.js
@@ -2,31 +2,98 @@
 
 // create an array with all possible character types: Uppercase, Lowercase, numbers, and symbols stored in a variable
 // create a global variable called "pwLength" with a number between 10 and 18
+const characters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "_",
+  "*",
+  "&",
+  "$"
+];
+
+let pwLength = 13;
+const addNewPassword = () => {
+  let passwordHolder = "";
+  const characterLength = characters.length;
+
+  for (let i = 0; i < pwLength; i++) {
+    passwordHolder += characters[Math.floor(Math.random() * characters.length)];
+  }
+
+  return passwordHolder;
+};
+addNewPassword();
 
 // Using the above array and password length variable, create a random password using a for loop inside of a function called "addNewPassword" either saved as an arrow function variable or a traditional function
-function myFunction() {
-    let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZacdefghijklnopqrstuvwxyz0123456789@#$%^&*";
-  
-  // let letters = "0123456789"
-    password = ""; 
-    
-    for (var i = 0;i < 20;i++) {
-      let generate = letters[Math.floor(Math.random() * 62)];
-      password += generate;
-    }
-    
-       
-    
-  let parent = document.getElementById("password").innerHTML = password;
-  }
-    
+
 // ========= ⬇ DO NOT TOUCH THIS CODE ⬇ ======
 
 let genBtn = document.getElementById("btnGen");
 let buttonHandler = () => {
-    document.getElementById("password").value = addNewPassword();
+  document.getElementById("password").value = addNewPassword();
 };
 // Event listener for generate PW button
 genBtn.addEventListener("click", buttonHandler);
 
 // ========= ⬆ DO NOT TOUCH THIS CODE ⬆ ======
+
